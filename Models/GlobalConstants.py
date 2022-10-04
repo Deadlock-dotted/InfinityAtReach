572593424554fcb835ee017d210f95cf341ac453
suhil.roshan <suhilroshan010@gmail.com>
2022-10-04 23:14:33 +0530

    suggestions API changes

---
+++ Models/GlobalConstants.py
@@ -0,0 +1,3 @@
+Create_New_Policy = 'I want to buy a new Home Insurance Policy'
+Policy_Suggestion = 'Suggest me a Home Insurance Policy'
+Basic_Insurance_Details = 'Help me with basic insurance details and keywords'
--- app.py
+++ app.py
@@ -1,5 +1,8 @@
+from Models.GlobalConstants import Create_New_Policy, Policy_Suggestion, Basic_Insurance_Details
 from chatbot import chatbot
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
+
+import pypyodbc
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -12,9 +15,34 @@ def home():
 
 @app.route("/get")
 def get_bot_response():
-    userText = request.args.get('msg')
-    return str(chatbot.get_response(userText))
+    usertext = request.args.get('msg')
+    if usertext == Create_New_Policy:
+        return redirect(url_for('database'))
+    if usertext == Policy_Suggestion:
+        return redirect(url_for('getuserinformation'))
+    if usertext == Basic_Insurance_Details:
+        return redirect(url_for('needtodo'))
+    return str(chatbot.get_response(usertext))
+
+
+@app.route("/database")
+def database():
+    connection = pypyodbc.connect('Driver={SQL Server};Server=REM-HMB0GG3;Database=DEMO;')
+    cursor = connection.cursor()
+    cursor.execute("SELECT * FROM demotable")
+    print(cursor)
+    print(cursor.rowcount)
+
+    for row in cursor:
+        print(row)
+
+    return str(row)
+
+
+@app.route("database")
+def getuserinformation():
+    return str('hi')
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
