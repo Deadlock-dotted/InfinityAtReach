f287d9426f81d5c9f6cfda76ab8e7e25e0675a20
suhil.roshan <suhilroshan010@gmail.com>
2022-10-12 11:52:26 +0530

    Added Plot Visualisation API

--- Models/GlobalConstants.py
+++ Models/GlobalConstants.py
@@ -1,3 +1,4 @@
 Create_New_Policy = 'I want to buy a new Home Insurance Policy'
 Policy_Suggestion = 'Suggest me a Home Insurance Policy'
 Basic_Insurance_Details = 'Help me with basic insurance details and keywords'
+Descriptive_Statistics = 'Show me Descriptive Statistics'
--- app.py
+++ app.py
@@ -1,18 +1,19 @@
+import io
+
 from numpy.compat import basestring
 
-from Models.GlobalConstants import Create_New_Policy, Policy_Suggestion, Basic_Insurance_Details
+from Models.GlobalConstants import Create_New_Policy, Policy_Suggestion, Basic_Insurance_Details, Descriptive_Statistics
 from chatbot import chatbot
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, send_file
 import pandas as pd
 import numpy as np
 import seaborn as sns
+import matplotlib.pyplot as plt
 
 from datetime import timedelta
 from flask import make_response, request, current_app
 from functools import update_wrapper
-
 from flask_cors import CORS, cross_origin
-
 import pypyodbc
 
 app = Flask(__name__)
@@ -24,6 +25,8 @@ Salaried = 1
 CoverType = 'Structure & Content'
 Tenure = 10
 OutParam = ''
+country = 'India'
+data = pd.read_excel('C:/Users/suhil.roshan/Downloads/ExportExcel (1).xlsx')
 
 
 def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
@@ -99,6 +102,8 @@ def get_bot_response():
                                 Salaried=Salaried,
                                 CoverType=CoverType,
                                 Tenure=Tenure))
+    if usertext == Descriptive_Statistics:
+        return redirect(url_for('DescriptiveStatistics'))
     if usertext == Basic_Insurance_Details:
         return redirect(url_for('needtodo'))
     if usertext == Basic_Insurance_Details:
@@ -171,6 +176,27 @@ def PolicyRating():
 @app.route("/HousePricePrediction")
 def HousePricePrediction():
     return str('')
+
+
+@app.route("/DescriptiveStatistics")
+def DescriptiveStatistics():
+    dataframe = pd.DataFrame(data)
+    dataframe2 = dataframe[dataframe['Country'] == country]
+
+    plt.figure(figsize=(10, 8))
+    plt.xticks(rotation='vertical')
+
+    visualisation = sns.barplot(x=dataframe2['City'], y=dataframe2['BeenCustomer'])
+
+    # plt.show(), plt.savefig was not working, getting the typeerror
+    # returning visualisation is 200 status but preview is not seen in UI
+
+    # Converting to bytes -- Working perfectly
+    bytes_image = io.BytesIO()
+    plt.savefig(bytes_image, format='png')
+    bytes_image.seek(0)
+
+    return send_file(bytes_image, mimetype='image/png')
 
 
 if __name__ == "__main__":
